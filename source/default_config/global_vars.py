dcafc7cadba5534cc871c88025f1fbe72be09354
Yu-Yuan Yang at marvin (AF-lab) <yu-yuan.yang@qmul.ac.uk>
2024-01-17 22:24:11 +0000

    update global_vars path as a submodule

--- source/default_config/global_vars.py
+++ source/default_config/global_vars.py
@@ -14,45 +14,47 @@ epsilon = 1.0e-6
 config = configparser.ConfigParser()
 path_args = __file__.split("/")[0:-1]
 root_path = "/".join(path_args)
-config.read(f"{root_path}/config.cfg")
+# config.read(f"{root_path}/config.cfg")            # serve as a main module
+config.read(f"{root_path}/../../../../config.cfg")  # serve as a submodule
 config.sections()
 
 # Set the environment variables for the programs used by MaSIF.
 msms_bin = ""
-if "MSMS_BIN" in config["Default"]:
-    msms_bin = config["Default"]["MSMS_BIN"]
+if "MSMS_BIN" in config["ThirdParty"]:
+    msms_bin = config["ThirdParty"]["MSMS_BIN"]
 else:
     set_trace()
     print("ERROR: MSMS_BIN not set. Variable should point to MSMS program.")
     sys.exit(1)
 
 reduce_bin = ""
-if "REDUCE_BIN" in config["Default"]:
-    reduce_bin = config["Default"]["REDUCE_BIN"]
+if "REDUCE_BIN" in config["ThirdParty"]:
+    reduce_bin = config["ThirdParty"]["REDUCE_BIN"]
 else:
     print("ERROR: REDUCE_BIN not set. Variable should point to REDUCE program.")
     sys.exit(1)
 
 pdb2pqr_bin = ""
-if "PDB2PQR_BIN" in config["Default"]:
-    pdb2pqr_bin = config["Default"]["PDB2PQR_BIN"]
+if "PDB2PQR_BIN" in config["ThirdParty"]:
+    pdb2pqr_bin = config["ThirdParty"]["PDB2PQR_BIN"]
 else:
     print("ERROR: PDB2PQR_BIN not set. Variable should point to PDB2PQR_BIN program.")
     sys.exit(1)
 
 apbs_bin = ""
-if "APBS_BIN" in config["Default"]:
-    apbs_bin = config["Default"]["APBS_BIN"]
+if "APBS_BIN" in config["ThirdParty"]:
+    apbs_bin = config["ThirdParty"]["APBS_BIN"]
 else:
     print("ERROR: APBS_BIN not set. Variable should point to APBS program.")
     sys.exit(1)
 
 multivalue_bin = ""
-if "MULTIVALUE_BIN" in config["Default"]:
-    multivalue_bin = config["Default"]["MULTIVALUE_BIN"]
+if "MULTIVALUE_BIN" in config["ThirdParty"]:
+    multivalue_bin = config["ThirdParty"]["MULTIVALUE_BIN"]
 else:
     print("ERROR: MULTIVALUE_BIN not set. Variable should point to MULTIVALUE program.")
     sys.exit(1)
+
 
 class NoSolutionError(Exception):
     pass
