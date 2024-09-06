afb4de99be5985c391aadbe60cf0fc1c449570cd
Yu-Yuan (Stuart) Yang - from connectin, AFLab <yu-yuan.yang@qmul.ac.uk>
2024-09-06 17:19:33 +0100

    MSMS probe dynamic adjustment to prevent rare dumps

--- source/triangulation/computeMSMS.py
+++ source/triangulation/computeMSMS.py
@@ -22,28 +22,29 @@ def computeMSMS(pdb_file, protonate=True):
     else:
         print("Error - pdb2xyzrn is deprecated.")
         sys.exit(1)
-    # Now run MSMS on xyzrn file
-    FNULL = open(os.devnull, "w")
-    args = [
-        msms_bin,
-        "-density",
-        "3.0",
-        "-hdensity",
-        "3.0",
-        "-probe",
-        "1.5",
-        "-if",
-        out_xyzrn,
-        "-of",
-        file_base,
-        "-af",
-        file_base,
-    ]
-    # print msms_bin+" "+`args`
-    p2 = Popen(args, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p2.communicate()
 
-    vertices, faces, normals, names = read_msms(file_base)
+    # Now run MSMS on xyzrn file and Try to read ths msms file
+    # if it fails, try again with a different probe radius.
+    pr_dynamic_adjust = [1.5, 1.499, 1.501, 1.49, 1.51]
+    for idx, probe_radius in enumerate(pr_dynamic_adjust):
+        try:
+            run_msms(out_xyzrn, file_base, probe_radius)
+            vertices, faces, normals, names = read_msms(file_base)
+        except Exception as e:
+            print(f"[Warning]: {e}")
+            print(
+                f"[Warning]: running errors with probe radius {probe_radius} in MSMS.",
+            )
+            if idx < len(pr_dynamic_adjust):
+                print(f"Trying another parameter = {pr_dynamic_adjust[idx+1]}.")
+            else:
+                print("Error - All candidate probe radius failed. Exiting.")
+                sys.exit(1)
+            continue
+        else:
+            break
+
+    # Read the areas file
     areas = {}
     ses_file = open(file_base + ".area")
     next(ses_file)  # ignore header line
@@ -56,4 +57,31 @@ def computeMSMS(pdb_file, protonate=True):
     os.remove(file_base + ".xyzrn")
     os.remove(file_base + ".vert")
     os.remove(file_base + ".face")
+
     return vertices, faces, normals, names, areas
+
+
+def run_msms(out_xyzrn, file_base, probe_radius=1.5):
+    FNULL = open(os.devnull, "w")
+    # string with 3 digits after the decimal point.
+    probe_radius = "{:.3f}".format(probe_radius)
+    args = [
+        msms_bin,
+        "-density",
+        "3.0",
+        "-hdensity",
+        "3.0",
+        "-probe",
+        probe_radius,
+        "-if",
+        out_xyzrn,
+        "-of",
+        file_base,
+        "-af",
+        file_base,
+    ]
+    # print msms_bin+" "+`args`
+    p2 = Popen(args, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p2.communicate()
+
+    return None
