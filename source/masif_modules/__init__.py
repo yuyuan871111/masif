45bb370104c0cddbb50e88f9445ea7a02a18792c
Yu-Yuan (Stuart) Yang from connectin - AFLab <yu-yuan.yang@qmul.ac.uk>
2026-02-10 18:17:16 +0000

    add license header since they are formatted by a formatter and regarded as modified and remove zip

---
+++ source/masif_modules/__init__.py
@@ -0,0 +1,20 @@
+# MaSIF (Molecular Surface Interaction Fingerprints)
+# Fork with modifications: https://github.com/yuyuan871111/masif
+# Upstream: https://github.com/LPDI-EPFL/masif
+#
+# This file has been modified in this fork.
+#
+# Original work Copyright (c) 2019 Gainza P, Sverrisson F, Monti F, Rodola,
+# Bronstein MM, Correia BE
+# Modifications Copyright (c) 2024-2026 Yu-Yuan (Stuart) Yang /
+# Arianna Fornili's Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# NOTICE: This product includes software developed by the MaSIF authors and contributors.
+#
+# (Optional) Change log / compare view:
+# https://github.com/LPDI-EPFL/masif/compare/master...yuyuan871111:masif:master
